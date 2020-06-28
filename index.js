2e6b63503ba2d6a3f2911ed5f5780872f5618ca2
Hussein Talal <hossientalal2@gmail.com>
2020-06-28 15:42:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function mapToNoChange(sourceArray){
   sourceArray.forEach(elem => result.push(elem ));
   return result;
 }
+function mapToDouble(sourceArray){
+  const result = [];
+  sourceArray.forEach(elem => result.push(elem * 2));
+  return result;
+}
+function mapToSquare(sourceArray){
+  const result = [];
+  sourceArray.forEach(elem => result.push(elem * elem));
+  return result;
+}
